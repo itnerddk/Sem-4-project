4ea3d9ce02ccafa06a393f4263477c3eb0123788
PausedMagician <75096283+PausedMagician@users.noreply.github.com>
2025-03-07 11:47:48 +0100

    Remove unused import
    Co-authored-by: JustAVirus <ActuallyJustAVirus@users.noreply.github.com>
    Co-authored-by: Liem <LiemQLe@users.noreply.github.com>

--- TestMission/src/main/java/org/sdu/sem4/g7/testmission/TestMissionPlugin.java
+++ TestMission/src/main/java/org/sdu/sem4/g7/testmission/TestMissionPlugin.java
@@ -4,7 +4,6 @@ import static java.util.stream.Collectors.toList;
 import java.util.Collection;
 import java.util.ServiceLoader;
 
-import org.sdu.sem4.g7.common.data.Entity;
 import org.sdu.sem4.g7.common.data.GameData;
 import org.sdu.sem4.g7.common.data.Mission;
 import org.sdu.sem4.g7.common.services.IEntityPluginService;
