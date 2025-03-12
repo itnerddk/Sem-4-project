1ca01336525c6e792f98aa481cc7dae5d1308de9
PausedMagician <75096283+PausedMagician@users.noreply.github.com>
2025-03-12 13:09:59 +0100

    Rename to mission

--- Common/src/main/java/org/sdu/sem4/g7/common/services/IEntityPluginService.java
+++ Common/src/main/java/org/sdu/sem4/g7/common/services/IEntityPluginService.java
@@ -5,7 +5,7 @@ import org.sdu.sem4.g7.common.data.Mission;
 
 public interface IEntityPluginService {
 
-    void start(GameData gameData, Mission world);
+    void start(GameData gameData, Mission mission);
 
-    void stop(GameData gameData, Mission world);
+    void stop(GameData gameData, Mission mission);
 }
