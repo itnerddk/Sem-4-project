1d5d8638e810dd92c417b50de4bf0ca861261570
Mikkel <mikkel@itnerd.dk>
2025-03-17 14:55:39 +0100

    Jackson now works with the MissionLoader module

--- MissionLoader/src/main/java/module-info.java
+++ MissionLoader/src/main/java/module-info.java
@@ -3,6 +3,9 @@ import org.sdu.sem4.g7.common.services.IGamePluginService;
 
 module MissionLoader {
     requires Common;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.databind;
+    exports org.sdu.sem4.g7.MissionLoader.objects to com.fasterxml.jackson.databind;
     provides IGamePluginService with org.sdu.sem4.g7.MissionLoader.services.Loader;
 }
