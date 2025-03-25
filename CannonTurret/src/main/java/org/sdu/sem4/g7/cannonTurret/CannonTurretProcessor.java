3ecbfd3e645e909f1a70b1c335676974b8cfcb40
Mikkel <mikkel@itnerd.dk>
2025-03-25 18:27:15 +0100

    Bullets damages entities it hit
    NOTE: Work in progress, bullet currently damages the tank shooting. Due to it colliding with it.

--- CannonTurret/src/main/java/org/sdu/sem4/g7/cannonTurret/CannonTurretProcessor.java
+++ CannonTurret/src/main/java/org/sdu/sem4/g7/cannonTurret/CannonTurretProcessor.java
@@ -1,6 +1,7 @@
 package org.sdu.sem4.g7.cannonTurret;
 
 import org.sdu.sem4.g7.common.data.Entity;
+import org.sdu.sem4.g7.common.services.ICollidableService;
 import org.sdu.sem4.g7.common.services.IEntityProcessingService;
 
 import org.sdu.sem4.g7.common.data.GameData;
@@ -9,6 +10,23 @@ import org.sdu.sem4.g7.common.data.Vector2;
 import org.sdu.sem4.g7.tank.parts.Turret;
 
 public class CannonTurretProcessor implements IEntityProcessingService {
+
+    /**
+     * Check if the two entities is colliding
+     * 
+     * @param entity1
+     * @param entity2
+     * @return true if the overlap
+     */
+    private boolean collision(Entity bullet, Entity entity) {
+        if (entity instanceof ICollidableService) {
+            return bullet.getPosition().getX() < entity.getPosition().getX() + 60 &&
+                bullet.getPosition().getX() + 60 > entity.getPosition().getX() &&
+                bullet.getPosition().getY() < entity.getPosition().getY() + 60 &&
+                bullet.getPosition().getY() + 60 > entity.getPosition().getY();
+        }
+        return false;
+    }
 
 	@Override
 	public void process(GameData gameData, WorldData mission) {
@@ -23,6 +41,25 @@ public class CannonTurretProcessor implements IEntityProcessingService {
             
             // System.out.println("Bullet velocity: " + velocity);
             cb.setPosition(velocity.getX() + cb.getPosition().getX(), velocity.getY() + cb.getPosition().getY());            
+        }
+
+        // check for entities getting hit
+        for (Entity bullet : mission.getEntities(CannonBullet.class)) {
+
+            for (Entity e : mission.getEntities()) {
+
+                // skip itself
+                if (bullet.equals(e)) { continue; }
+
+                // check collission
+                if (collision(bullet, e)) {
+                    //System.out.println("Bullet: " + bullet.getID() + " Hit: " + e.getID() + " (" + e.getClass().getSimpleName() + ")");
+                    CannonBullet cannonBullet = (CannonBullet) bullet;
+                    e.setHealth(e.getHealth() - cannonBullet.getDamage());
+                }
+
+            }
+
         }
 	}
 }
