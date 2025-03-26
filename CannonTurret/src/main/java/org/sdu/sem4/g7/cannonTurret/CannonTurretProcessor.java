1e53cfe95e37dd25fea76563a59b9ba8afe88bfe
Mikkel <mikkel@itnerd.dk>
2025-03-26 20:48:13 +0100

    Despawn bullets when they leave the map

--- CannonTurret/src/main/java/org/sdu/sem4/g7/cannonTurret/CannonTurretProcessor.java
+++ CannonTurret/src/main/java/org/sdu/sem4/g7/cannonTurret/CannonTurretProcessor.java
@@ -30,24 +30,30 @@ public class CannonTurretProcessor implements IEntityProcessingService {
     }
 
 	@Override
-	public void process(GameData gameData, WorldData mission) {
+	public void process(GameData gameData, WorldData worldData) {
 		// Implementation of the process method
-        for (Entity e : mission.getEntities(CannonTurret.class)) {
+        for (Entity e : worldData.getEntities(CannonTurret.class)) {
             Turret t = (Turret) e;
         }
 
-        for (Entity e : mission.getEntities(CannonBullet.class)) {
+        for (Entity e : worldData.getEntities(CannonBullet.class)) {
             CannonBullet cb = (CannonBullet) e;
             Vector2 velocity = cb.getVelocity();
             
             // System.out.println("Bullet velocity: " + velocity);
-            cb.setPosition(velocity.getX() + cb.getPosition().getX(), velocity.getY() + cb.getPosition().getY());            
+            cb.setPosition(velocity.getX() + cb.getPosition().getX(), velocity.getY() + cb.getPosition().getY());  
+            
+            // despawn bullet if outside the map
+            if (e.getPosition().getX() > gameData.getMissionLoaderService().getMapSizeX() || e.getPosition().getY() > gameData.getMissionLoaderService().getMapSizeY() ||
+                e.getPosition().getX() < 0 || e.getPosition().getY() < 0) { 
+                worldData.removeEntity(e);
+            }
         }
 
         // check for entities getting hit
-        for (Bullet bullet : mission.getEntities(CannonBullet.class)) {
+        for (Bullet bullet : worldData.getEntities(CannonBullet.class)) {
 
-            for (Entity e : mission.getEntities()) {
+            for (Entity e : worldData.getEntities()) {
 
                 // skip itself
                 if (bullet.equals(e)) { continue; }
