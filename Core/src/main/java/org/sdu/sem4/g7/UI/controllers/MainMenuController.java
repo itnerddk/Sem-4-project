82f8e3755faa8d1200ce6b1723c797b9754a67dd
playerjones <jonasbossen99@hotmail.com>
2025-03-28 11:51:27 +0100

    Main Menu wip
    Work in progress main menu

--- Core/src/main/java/org/sdu/sem4/g7/UI/controllers/MainMenuController.java
+++ Core/src/main/java/org/sdu/sem4/g7/UI/controllers/MainMenuController.java
@@ -2,12 +2,27 @@ package org.sdu.sem4.g7.UI.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import org.sdu.sem4.g7.main.GameInstance;
 
-public class MainMenuController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuController implements Initializable {
+
+    @FXML
+    private ImageView backgroundImage;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Image img = new Image(getClass().getResource("/images/image.png").toExternalForm());
+        backgroundImage.setImage(img);
+    }
 
     @FXML
     private void handleStartGame(ActionEvent event) {
@@ -18,5 +33,17 @@ public class MainMenuController {
         stage.setScene(gameScene);
         stage.setTitle("Tank Wars");
         stage.show();
+    }
+
+    public void handleSettings(ActionEvent actionEvent) {
+
+    }
+
+    public void handleCredits(ActionEvent actionEvent) {
+
+    }
+
+    public void handleQuitGame(ActionEvent actionEvent) {
+        System.exit(0);
     }
 }
