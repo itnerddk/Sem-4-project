6959d31579e94b4643b1f61bc5627dff903cc45d
JustAVirus <91348480+NotJustAVirus@users.noreply.github.com>
2025-05-16 09:08:34 +0200

    Fix unit tests

--- Common/src/test/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module CommonTest {
-    requires Common;
-    requires org.junit.jupiter.api; // Ignore this is "missing", it's not being very smart. If removed it throws errors as it can't find it.
-    exports org.sdu.sem4.g7.common.tests to org.junit.platform.commons;
-
-    opens org.sdu.sem4.g7.common.tests to org.junit.platform.commons;
-}
